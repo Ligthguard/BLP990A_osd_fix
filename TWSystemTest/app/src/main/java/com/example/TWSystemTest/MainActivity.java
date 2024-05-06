abdf9627e12ebb871878d9fe8c91b6e7b0e4a82d
Lightguard <Lightguard@tauri.hu>
2024-05-06 09:41:39 +0200

    Initial commit

---
+++ TWSystemTest/app/src/main/java/com/example/TWSystemTest/MainActivity.java
@@ -0,0 +1,131 @@
+package com.example.TWSystemTest;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import android.tw.john.TWUtil;
+import android.view.View;
+import android.widget.Switch;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    TWUtilTest m_test = null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public void attachHandler(View view)
+    {
+        if(m_test != null)
+        {
+            m_test.AttachHandler();
+        }
+        else
+            AppendLogText("/dev/tw was not opened yet!");
+    }
+
+    public void openDeviceWithCustomValuesOnClick(View view)
+    {
+        try
+        {
+            if(m_test == null)
+                m_test = new TWUtilTest(this);
+
+            boolean isRangeMode = ((Switch)findViewById(R.id.rangeModeSwitch)).isChecked();
+            String _values = ((TextView) findViewById(R.id.idList)).getText().toString();
+            String[] parts = _values.split(" ");
+
+            if(!isRangeMode)
+            {
+                short[] tempValues = new short[parts.length];
+                for (int i = 0; i < parts.length; ++i)
+                    tempValues[i] = Short.parseShort(parts[i]);
+
+                AppendLogText("Opening /dev/tw with values: " + _values);
+                m_test.open(tempValues);
+            }
+            else
+            {
+                if(parts.length < 2)
+                {
+                    AppendLogText("Invalid values for range start/end.");
+                    return;
+                }
+
+                short start = Short.parseShort(parts[0]);
+                short end = Short.parseShort(parts[1]);
+
+                if(end >= start)
+                {
+                    AppendLogText("Range start should be smaller than range end.");
+                    return;
+                }
+
+                short[] values = new short[end - start + 1];
+                for(short i=start; i<=end; ++i)
+                    values[i] = i;
+
+                AppendLogText("Opening /dev/tw with values: " + _values);
+                m_test.open(values);
+            }
+        }
+        catch (Exception e)
+        {
+            AppendLogText(e.getMessage());
+        }
+    }
+
+    public void closeDevice(View view)
+    {
+        if(m_test != null)
+        {
+            m_test.close();
+            AppendLogText("Closed /dev/tw.");
+        }
+    }
+
+    public void toggleHVACScreen(View view)
+    {
+        if(m_test == null)
+            m_test = new TWUtilTest(this);
+
+        try
+        {
+            m_test.TryToggleHVACScreen();
+        }
+        catch (Exception e)
+        {
+            AppendLogText(e.getMessage());
+        }
+    }
+
+    public void clearLog(View view)
+    {
+        TextView v = (TextView)findViewById(R.id.textView);
+        if(v != null)
+            v.setText("");
+    }
+
+    public void AppendLogText(String str)
+    {
+        str += "\r\n";
+        TextView v = (TextView)findViewById(R.id.textView);
+        if(v != null)
+            v.append(str);
+    }
+}
---
+++ TWSystemTest/app/src/main/java/com/example/TWSystemTest/TWUtilTest.java
@@ -0,0 +1,81 @@
+package com.example.TWSystemTest;
+
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.os.Handler;
+import android.os.Message;
+import android.tw.john.TWUtil;
+
+public class TWUtilTest
+{
+    private final TWUtil m_TW = new TWUtil();
+    private boolean m_openSuccess = false;
+    static MainActivity m_activity = null;
+    public TWUtilTest(MainActivity activity)
+    {
+        m_activity = activity;
+    }
+
+    @SuppressLint("HandlerLeak")
+    private static final Handler mHandler = new Handler()
+    {
+        public void handleMessage(Message msg)
+        {
+            try
+            {
+                String out = "op: " + String.valueOf(msg.what) + " arg1: " + String.valueOf(msg.arg1) + " arg2: " + String.valueOf(msg.arg2);
+                m_activity.AppendLogText(out);
+            }
+            catch (Exception e)
+            {
+                m_activity.AppendLogText(e.getMessage());
+            }
+        }
+    };
+
+    public void AttachHandler()
+    {
+        if(m_openSuccess)
+        {
+            m_TW.addHandler("TWUtilTest", mHandler);
+            m_TW.start();
+            m_activity.AppendLogText("Handler attached and started!");
+        }
+        else
+            m_activity.AppendLogText("Attach: Device not open!");
+    }
+
+    public void open(short[] args)
+    {
+        int result = m_TW.open(args);
+        if(result != 0)
+        {
+            AlertDialog.Builder dlgAlert  = new AlertDialog.Builder(m_activity);
+            dlgAlert.setMessage("Failed to open /dev/tw with these params.");
+            dlgAlert.setTitle("Failed to open /dev/tw");
+            dlgAlert.setPositiveButton("OK", null);
+            dlgAlert.setCancelable(true);
+            dlgAlert.create().show();
+        }
+
+        m_activity.AppendLogText("Successfully opened /dev/tw!");
+        m_openSuccess = true;
+    }
+
+    public void close()
+    {
+        if(m_openSuccess)
+            m_TW.close();
+
+        m_openSuccess = false;
+    }
+
+    public void TryToggleHVACScreen()
+    {
+        if(m_TW != null)
+        {
+            m_TW.write(1281, 170, 85);
+            m_activity.AppendLogText("Sending 1281 170 85 to /dev/tw!");
+        }
+    }
+}
